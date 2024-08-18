dd12aa87113119c9f6ec36a7a5ec66c86007aa20
NgocNg2910 <92688407+NgocNg2910@users.noreply.github.com>
2024-08-18 12:17:51 -0700

    Moving car

--- BuiltToScaleProj/Assets/Scripts/MoveLeftRight.cs
+++ BuiltToScaleProj/Assets/Scripts/MoveLeftRight.cs
@@ -6,16 +6,27 @@ public class MoveLeftRight : MonoBehaviour
 {
     public float moveSpeed = 5;
     Vector3 pos;
+    public float secondUntilRestart;
+    public bool randomizeRestartTime;
+    float storedRestartTime;
+    public Vector2 minMaxRandom;
 
     private void Start()
     {
         pos = transform.position;
+        storedRestartTime = secondUntilRestart;
     }
 
     private void FixedUpdate()
     {
         transform.position += new Vector3(moveSpeed, 0, 0) * Time.fixedDeltaTime;
-        if (transform.position.x > (pos.x + 15))
+        if (transform.position.x > (pos.x + secondUntilRestart))
+        {
             transform.position = pos;
+            if (randomizeRestartTime)
+            {
+                secondUntilRestart = storedRestartTime + Random.Range(minMaxRandom.x, minMaxRandom.y);
+            }
+        }
     }
 }

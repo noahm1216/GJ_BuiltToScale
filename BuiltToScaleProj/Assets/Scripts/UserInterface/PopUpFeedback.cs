d66ebbea6b91a5bc14fba067d9555f3adbfacce0
Noah <16135831+noahm1216@users.noreply.github.com>
2024-08-19 20:15:29 -0700

    Narrative code updates

--- BuiltToScaleProj/Assets/Scripts/UserInterface/PopUpFeedback.cs
+++ BuiltToScaleProj/Assets/Scripts/UserInterface/PopUpFeedback.cs
@@ -54,6 +54,29 @@ public class PopUpFeedback : MonoBehaviour
             //ToyGuard
             //Chest
             //Boombox
+            switch (name)
+            {
+                case "Sky":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Sky, NarrativeHolder.InteractType.Click);
+                    break;
+                case "Monster":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Monster, NarrativeHolder.InteractType.Click);
+                    break;
+                case "FlagPole":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.FlagPole, NarrativeHolder.InteractType.Click);
+                    break;
+                case "ToyGuard":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.ToyGuard, NarrativeHolder.InteractType.Click);
+                    break;
+                case "Chest":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Chest, NarrativeHolder.InteractType.Click);
+                    break;
+                case "Boombox":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Boombox, NarrativeHolder.InteractType.Click);
+                    break;
+                default:
+                    break;
+            }
         }
         else if (messageType == Message.Collect)
         {
@@ -63,19 +86,64 @@ public class PopUpFeedback : MonoBehaviour
             //Moon
             //RickRoll
             //DancingSquad
+            switch (name)
+            {
+                case "Key":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Key, NarrativeHolder.InteractType.Collect);
+                    break;
+                case "Flag":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Flag, NarrativeHolder.InteractType.Collect);
+                    break;
+                case "Monster Toy":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.MonsterToy, NarrativeHolder.InteractType.Collect);
+                    break;
+                case "Moon":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Moon, NarrativeHolder.InteractType.Collect);
+                    break;
+                case "RickRoll":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.RickRoll, NarrativeHolder.InteractType.Collect);
+                    break;
+                case "DancingSquad":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.DancingSquad, NarrativeHolder.InteractType.Collect);
+                    break;
+                default:
+                    break;
+            }
         }
         else if (messageType == Message.Interaction)
         {
-            //Sky
-            //Monster
-            //FlagPole
-            //ToyGuard
-            //Chest
-            //Boombox
+            //Sky (with moon)
+            //Monster (with dragon toy)
+            //FlagPole (with toy flagpole)
+            //Chest (with key)
+            //Boombox (DancingSquad)
+            switch (name)
+            {
+                case "Sky":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Sky, NarrativeHolder.InteractType.Interaction);
+                    break;
+                case "Monster":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Monster, NarrativeHolder.InteractType.Interaction);
+                    break;
+                case "FlagPole":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.FlagPole, NarrativeHolder.InteractType.Interaction);
+                    break;
+                case "Chest":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Chest, NarrativeHolder.InteractType.Interaction);
+                    break;
+                case "Boombox":
+                    StringToDisplay = ref_NarrativeHolder.PickRandomInteractionLine(NarrativeHolder.InteractionObject.Boombox, NarrativeHolder.InteractType.Interaction);
+                    break;
+                default:
+                    break;
+            }
         }
 
         if (feedbackTextBox)
             feedbackTextBox.text = StringToDisplay;
+
+        if (logInMessages.Instance)
+            logInMessages.Instance.SendMessage(StringToDisplay); // sends the log to chat history
 
         EnableUI();
     }
